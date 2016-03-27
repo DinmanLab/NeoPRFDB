78cc08561494fad0ec95bb5522e0a5def20cad7e
abelew <abelew@gmail.com>
2016-03-26 22:54:13 -0400

    empty dbixt

---
+++ lib/PRFdbix.pm
@@ -0,0 +1,42 @@
+package PRFNeo;
+## Time-stamp: <2016-03-26 22:52:21 abelew>
+use Moose;
+use common::sense;
+use diagnostics;
+use DBIx::Class;
+
+=head1 NAME
+
+PRFdbix - DBIx routines for the PRFNeo
+
+=head1 SYNOPSIS
+
+  use PRFNeo qw"";
+
+=head1 DESCRIPTION
+
+I have no clue what I am doing.
+
+=cut
+
+=head2 Methods
+
+=over 4
+
+
+
+=back
+
+=head1 AUTHOR - atb
+
+Email abelew@gmail.com
+
+=head1 SEE ALSO
+
+
+=cut
+
+1;
+
+__END__
+
